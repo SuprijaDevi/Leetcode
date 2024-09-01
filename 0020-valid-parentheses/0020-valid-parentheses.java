d75efe538d84c3e96a6f15801a383e3424886354
Suprija Devi <125337405+SuprijaDevi@users.noreply.github.com>
2024-09-01 23:17:39 +0530

    Time: 1 ms (98.72%), Space: 41.2 MB (85.35%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> valid = new Stack();
+
+        for(char c : s.toCharArray()) {
+            if(c == '(' || c == '[' || c == '{') {
+                valid.push(c);
+            }
+            else if(c == ')' || c == ']' || c == '}') {
+                if(valid.isEmpty())
+                return false;
+                char top = valid.pop();
+                if((c == ')' && top != '(') ||
+                   (c == ']' && top != '[') ||
+                   (c == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
+        return valid.isEmpty();
+    }
+}
