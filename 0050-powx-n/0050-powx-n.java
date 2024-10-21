16461fdd4f886e7d629f88a4029e02b39c0d770f
Suprija Devi <125337405+SuprijaDevi@users.noreply.github.com>
2024-10-21 14:55:57 +0530

    Time: 0 ms (100%), Space: 42.1 MB (59.99%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,21 @@
 class Solution {
+    public double power(double x, long n) {
+        if(n == 0) return 1;
+        if(n == 1) return x;
+        if(n % 2 == 0) {
+            return power(x * x, n / 2);
+        } else {
+            return x * power(x, (n - 1));
+        }
+    }
     public double myPow(double x, int n) {
-        return Math.pow(x, n);
+        if(n < 0) {
+            // x = 1 / x;
+            long m = (long)n;
+            m = -1 * m;
+            return 1/power(x, m);
+        } else {
+            return power(x, (long)n);
+        }
     }
 }
