f5b1a50e8884e8e2cd7e4debe6fb28363daedada
Suprija Devi <125337405+SuprijaDevi@users.noreply.github.com>
2024-10-21 11:00:13 +0530

    Time: 8 ms (50.24%), Space: 40.3 MB (38.45%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,12 +1,18 @@
 class Solution {
     public int fib(int n) {
-        int num1 = 0; int num2 = 1;
-        int num3 = 0;
-        for(int i=0; i<n; i++) {
-            num3 = num1 + num2;
-            num1 = num2;
-            num2 = num3;
-        }
-        return num1;
+        // int num1 = 0; int num2 = 1;
+        // int num3 = 0;
+        // for(int i=0; i<n; i++) {
+        //     num3 = num1 + num2;
+        //     num1 = num2;
+        //     num2 = num3;
+        // }
+        // return num1;
+        if(n <= 1) {
+            return n;
+        } 
+        int last = fib(n - 1);
+        int secLast = fib(n - 2);
+        return last + secLast;
     }
 }
