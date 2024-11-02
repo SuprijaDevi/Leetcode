20d7a141e234fee2cdda593411708a3117480ebc
Suprija Devi <125337405+SuprijaDevi@users.noreply.github.com>
2024-11-02 19:27:42 +0530

    Time: 15 ms (33.33%), Space: 70.2 MB (34.32%) - LeetHub

---
+++ 2728-sum-in-a-matrix/2728-sum-in-a-matrix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int matrixSum(int[][] nums) {
+        int row = nums.length;
+        int col = nums[0].length;
+        int sum = 0;
+        for(int[] rowArr : nums) {
+            Arrays.sort(rowArr);
+        }
+        for(int i=col-1; i>=0; i--) {
+            int max = 0;
+            for(int j=0; j<row; j++) {
+                max = Math.max(max, nums[j][i]);
+            }
+            sum += max;
+        }
+        return sum;
+    }
+}
