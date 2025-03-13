73a6dd994cb2cf55b4b6fe5d0debb39c062d2d53
Suprija Devi S <125337405+SuprijaDevi@users.noreply.github.com>
2025-03-13 23:06:47 +0530

    Time: 5 ms (41.02%), Space: 44.9 MB (16.25%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,15 +1,24 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for(int num : nums) {
+        TreeSet<Integer> set=new TreeSet<>();
+        for(int num:nums){
             set.add(num);
         }
-        List<Integer> sortedList = new ArrayList<>(set);
-        Collections.sort(sortedList, Collections.reverseOrder());
+        if(set.size()>=3){
+            return set.lower(set.lower(set.last()));
+        }
+        return set.last();
+        // Set<Integer> set = new HashSet<>();
+        // for(int num : nums) {
+        //     set.add(num);
+        // }
+        // List<Integer> sortedList = new ArrayList<>(set);
+        // Collections.sort(sortedList, Collections.reverseOrder());
 
-        if(sortedList.size() >= 3) 
-            return sortedList.get(2);
-        else
-            return sortedList.get(0);
+        // if(sortedList.size() >= 3) 
+        //     return sortedList.get(2);
+        // else
+        //     return sortedList.get(0);
+
     }
 }
